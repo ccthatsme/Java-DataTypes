3a38039f98f53f5e413703d4d1c8e05a5daa04f5
Chris <cciric44@gmail.com>
2019-09-30 17:33:59 -0400

    completed the substring challenge succesfully

--- src/SubstringComparison.java
+++ src/SubstringComparison.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 //Return the respective lexicographically smallest and largest 
 //substrings as a single newline-separated string.
@@ -7,10 +8,12 @@ import java.util.ArrayList;
 //3
 //3 letter groupings of welcome. 
 
+//this code below worked by inputing into an arraylist, sorting the arraylist and return
+//the first and last entry of it
 public class SubstringComparison {
 
 	public static void main(String[] args) {
-		String s = "welcome";
+		String s = "welcometojava";
 		char[] arr = s.toCharArray();
 		int k = 3;
 		
@@ -26,22 +29,35 @@ public class SubstringComparison {
 	 public static String getSmallestAndLargest(String s, int k) {
 	        String smallest = s.substring(0, k);
 	        String largest = "";
-	        //String sub = "";
+	        String sub = "";
 	        char[] arr = s.toCharArray();
 	        ArrayList<String> list = new ArrayList<>();
 	        
+	        for (int i = 0; i <= s.length()-(k); i=i+1){
+	        	sub = s.substring(i, i+k);
+	        	list.add(sub);
+	        
+	        }
+	        
+	        Collections.sort(list);
+	        smallest = list.get(0);
+	        largest = list.get(list.size()-1);
+	        
+	        
+	       
+	        
 
 	        
-	       for (int i = 0; i < s.length()-(k+1); i=i+1){
-	    	   if (s.substring(i, i+k).compareTo(smallest) < 0) {
-	    		   smallest = s.substring(i, i+k);
-	    	   }
-	    	   else if(s.substring(i, i+k).compareTo(largest) > 0) {
-	    		   largest = s.substring(i, i+k);
-	    	   }
-	       }
-	        
-	        return smallest + "\n" + largest;
+//	       for (int i = 0; i < s.length()-(k+1); i=i+1){
+//	    	   if (s.substring(i, i+k).compareTo(smallest) < 0) {
+//	    		   smallest = s.substring(i, i+k);
+//	    	   }
+//	    	   else if(s.substring(i, i+k).compareTo(largest) > 0) {
+//	    		   largest = s.substring(i, i+k);
+//	    	   }
+//	       }
+//	        
+	        return smallest + "\n" + largest + list;
 	    }
 
 }
